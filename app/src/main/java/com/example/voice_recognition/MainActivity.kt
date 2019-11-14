fdf5a4e9b549ea17446d01df54b08e115f020b3c
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-11-14 01:28:28 -0300

    modify main activity

--- app/src/main/java/com/example/voice_recognition/MainActivity.kt
+++ app/src/main/java/com/example/voice_recognition/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.voice_recognition
 
 import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.RecognizerIntent
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,9 +26,21 @@ class MainActivity : AppCompatActivity() {
         btnFalar = findViewById(R.id.btnFalar)
         txtTextoRecebido = findViewById(R.id.txtFalar)
 
-        //btnFalar.setOnClickListener(clickListenter)
+        btnFalar.setOnClickListener {
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
 
-        //override fun onClick()
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Fale algo normalmente")
+
+            try {
+                startActivityForResult(intent, ID_TEXTO_VOZ)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(this, "Este dispositivo não suporta comando de voz...", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
